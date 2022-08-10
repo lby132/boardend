c9a121135159c3d4968ef916cb568c61cd583aca
pro <quddbscjswow@naver.com>
2022-08-11 02:00:04 +0900

    대댓글 순서로 인한 오류 해결

--- src/main/java/com/ex/login/model/UserVO.java
+++ src/main/java/com/ex/login/model/UserVO.java
@@ -1,18 +1,13 @@
 package com.ex.login.model;
 
-import com.sun.istack.internal.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 public class UserVO {
 
-    @NotEmpty
     private String userId;
-    @NotEmpty
     private String userPass;
     private String loginYn;
 }
